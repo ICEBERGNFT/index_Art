6d8edc6b5a3178c117e3035eab442c224ce7d093
ICEBERGNFT <ggexbaba@gmail.com>
2024-07-20 23:20:11 -0500

    Update index_Art.js

--- index_Art.js
+++ index_Art.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { Client } = require('pg');
 
 app.use(cors({
     origin: 'https://icebergnft.github.io', // Restringe los orígenes permitidos
